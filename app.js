9cecda22f6dbaf3996feab5fb5bbe1df52db5cc0
Saviour Essien <31137610+celebritydeveloper@users.noreply.github.com>
2022-03-08 18:09:32 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+import { Feedjet } from "https://cdn.jsdelivr.net/gh/celebritydeveloper/sp-widget/Feedjet.js";
+// import { feedblocks } from './query.js'
+
+//the feedblocks data from the graphQL server
+// const feedblocksData = feedblocks.data.listConsumerFeedblocks.data
+
+const feedjet = new Feedjet({position: "bottom-right"});
